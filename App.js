05bb93e88be4469b470ef3c8bfc0446c1356a745
MONITORES -TRABALHOS ETEC <165843427+trabalhos-etec@users.noreply.github.com>
2024-09-28 00:13:35 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+// Dependências do Navigation Container
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Telas importadas
 import Inicial from './screens/Inicial';
 import Register from './screens/Register';
 import Login from './screens/Login';
 import Home from './screens/Home';
+import Analysis from './screens/Analysis';
 
-const Stack = createStackNavigator();
+// Criação do stack
+const Stack = createNativeStackNavigator();
 
-function App() {
+// Navegação entre as telas
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Inicial">
-        <Stack.Screen name="Inicial" component={Inicial} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+        <Stack.Screen options={{ headerShown: false }} name="Inicial" component={Inicial} />
+        <Stack.Screen options={{ headerShown: false }} name="Register" component={Register} />
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+        <Stack.Screen options={{ headerShown: false }} name="Analysis" component={Analysis} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
